797035466db42dc133d77f81372b89ad7e22182a
ajibona <39883461+ajibona@users.noreply.github.com>
2024-07-11 04:37:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
+const toogle = document.querySelector('.toogle')
+const navigation = document.querySelector('.navigation')
 
-/* Varriable */
-let sugarCost = 25;
-let mangoCost = 50;
-
-let totalCost = sugarCost + mangoCost;
-let totalTimesCost = sugarCost * mangoCost;
-let totalMinusCost = mangoCost - sugarCost;
-let totalDivideCost = mangoCost / sugarCost;
-
-console.log(totalCost);
-console.log(totalTimesCost);
-console.log(totalMinusCost);
-console.log(totalDivideCost);
-
-
-console.log(typeof sugarCost);
-
-function dataBalnce(){
-  console.log(`hello databalance ${totalCost} ${totalMinusCost}`)
-}
-
-dataBalnce();
+toogle.addEventListener('click', () => {
+  toogle.classList.toggle('active')
+ navigation.classList.toggle('active')
+})
